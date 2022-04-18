63bf24adc8d3877cd2e523bf2837fb89cefe0578
Your Name <you@example.com>
2022-04-19 00:34:09 +0700

    DZ_lesson_12

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, render_template, send_from_directory
 # from functions import ...
+from main.main import main_blueprint
+from loader.loader import loader_blueprint
 
 POST_PATH = "posts.json"
-UPLOAD_FOLDER = "uploads/images"
+UPLOAD_FOLDER = "uploads/images/"
 
 app = Flask(__name__)
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
 
 
 @app.route("/")
@@ -27,9 +31,9 @@ def page_post_upload():
     pass
 
 
-@app.route("/uploads/<path:path>")
+@app.route("/uploads/images/<path:path>")
 def static_dir(path):
-    return send_from_directory("uploads", path)
+    return send_from_directory(UPLOAD_FOLDER, path)
 
 
 app.run()
